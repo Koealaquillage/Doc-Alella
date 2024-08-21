6be41a5ac310f5e172baa7b9fce2685ea17fe86d
Koealaquillage <gkoenig.lamon@gmail.com>
2024-08-21 17:11:00 +0200

    Uses mammoth for reading .doc file and python-docx for .docx files

--- Database_interface.py
+++ Database_interface.py
@@ -1,10 +1,12 @@
 from pymongo import MongoClient
-from langchain_community.document_loaders import DirectoryLoader, DocxLoader
+from langchain_community.document_loaders import DirectoryLoader
 from langchain_community.llms import OpenAI
 from langchain.chains import RetrievalQA 
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import MongoDBAtlasVectorSearch
 import fitz  # PyMuPDF
+import mammoth  # For reading .doc files
+import docx  # For reading .docx files
 
 class DataBaseInterface:
 
@@ -55,22 +57,40 @@ class DataBaseInterface:
             text += page.get_text()
         return text
 
-    def load_docx_files(self, files):
-        docx_loader = DocxLoader()
+    def load_doc_files(self, files):
         data = []
         for file in files:
-            docx_data = docx_loader.load(file.name)
-            data.extend(docx_data)
+            text = self._extract_text_from_doc(file.name)
+            data.append(text)
         return data
+
+    def _extract_text_from_doc(self, doc_path):
+        with open(doc_path, "rb") as doc_file:
+            result = mammoth.convert_to_text(doc_file)
+            return result.value
+
+    def load_docx_files(self, files):
+        data = []
+        for file in files:
+            text = self._extract_text_from_docx(file.name)
+            data.append(text)
+        return data
+
+    def _extract_text_from_docx(self, docx_path):
+        doc = docx.Document(docx_path)
+        text = "\n".join([para.text for para in doc.paragraphs])
+        return text
 
     def load_all_files(self, files):
         txt_files = [file for file in files if file.name.endswith('.txt')]
         pdf_files = [file for file in files if file.name.endswith('.pdf')]
         docx_files = [file for file in files if file.name.endswith('.docx')]
+        doc_files = [file for file in files if file.name.endswith('.doc')]
 
         txt_data = self.load_txt_files(txt_files)
         pdf_data = self.load_pdf_files(pdf_files)
         docx_data = self.load_docx_files(docx_files)
+        doc_data = self.load_doc_files(doc_files)
 
-        all_data = txt_data + pdf_data + docx_data
+        all_data = txt_data + pdf_data + docx_data + doc_data
         return all_data
